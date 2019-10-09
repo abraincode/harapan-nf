8909bdacfae8ec17c9d802c409bd4b0eeba186d1
cookie1599 <atrisaendya@gmail.com>
2019-10-09 09:20:15 +0700

    menambahkan file renata48.php

---
+++ harapan/renata48.php
@@ -0,0 +1,6 @@
+<?php
+
+echo"Nama: Atrisa Endya Nur Hidayah<br/>";
+echo "Prodi: Teknik Informatika<br/>";
+echo "Semester: 5<br/>";
+echo "Harapan: Semoga STT Nurul Fikri menjadi Kampus yang dilirik oleh kalangan perusahan unicorn di indonesia";
